b0c0e3208f4f97f72325d5993bcca17439e5ab6b
LuongAnh <luonganh@gmail.com>
2025-03-14 16:38:32 +0700

    Update

--- src/Database/CreateDatabase_Linux.sql
+++ src/Database/CreateDatabase_Linux.sql
@@ -12,14 +12,3 @@ GO
 
 USE [OrderManagementDb]
 GO
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'app')
-BEGIN
-    EXEC ('CREATE SCHEMA app AUTHORIZATION dbo;');
-    PRINT 'Schema app created.';
-END
-ELSE
-BEGIN
-    PRINT 'Schema app already exists.';
-END
-GO
--- src/Database/Liquibase/migrations/002-add-users-table.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS app.users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
-);
